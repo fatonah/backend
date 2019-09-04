2044c37dab4e7e4addeaef92f2531aeaab2060a9
fatonah83 <54300924+fatonah83@users.noreply.github.com>
2019-09-04 16:52:30 +0800

    edit

--- resources/views/admin/transactions_users.blade.php
+++ resources/views/admin/transactions_users.blade.php
@@ -41,61 +41,9 @@
                                             }
                                            
                                     foreach ( $trans as $key => $arr_datas) {
- 
-									if(isset($arr_datas['fee'])){$fee = $arr_datas['fee'];}else{$fee=0;}
-									if($i==1){  
-
-											if(isset($arr_datas['category']) && $arr_datas['category']=='move'){ 										   
-										  $val = substr($arr_datas['amount'], 0, 1);  
-										  if($val=='-'){$fromacc = $arr_datas['accountx'] ; $toacc = $arr_datas['otheraccount'] ;}
-										  else{$fromacc = $arr_datas['otheraccount'] ; $toacc = $arr_datas['accountw']; }
-									   }
-									   
-										}else{
-                                       if(isset($arr_datas['category']) && $arr_datas['category']=='send'){
-										$balafter = $balafter +($arr_datas['amount']) + $fee;
-									   }elseif(isset($arr_datas['category']) && $arr_datas['category']=='receive'){
-										 $balafter = $balafter +($arr_datas['amount']) + $fee;  
-									   }elseif(isset($arr_datas['category']) && $arr_datas['category']=='move'){ 										   
-										  $val = substr($arr_datas['amount'], 0, 1);  
-										  if($val=='-'){$balafter = $balafter +($arr_datas['amount']) + $fee; $fromacc = $arr_datas['accountv'] ; $toacc = $arr_datas['otheraccount'] ;}
-										  else{$balafter = $balafter +($arr_datas['amount']) + $fee;  $fromacc = $arr_datas['otheraccount'] ; $toacc = $arr_datas['accountm']; }
-									   }
-										}
-                                            ?>  
-											
-                                            <tr>
-                                                <td><?php echo $i; ?></td> 
-                                          <?php if(isset($arr_datas['category']) && $arr_datas['category']=='receive'){ 
-					 $trans = gettransaction_crypto(strtoupper($crypto), $arr_datas['txid']); 
-					 ?>
-                                         <td><?php echo $trans['details'][0]['accountc']; ?></td>
-					 <?php }else if(isset($arr_datas['category']) && $arr_datas['category']=='move'){ ?>
-										<td><?php echo $fromacc; ?></td>
-					 <?php }else if(isset($arr_datas['category']) && $arr_datas['category']=='send'){ ?>
-										<td><?php echo $arr_datas['accounty']; ?></td>
-					 <?php }else if(!isset($arr_datas['category'])){ ?>
-                                         <td><?php echo $arr_datas['accountt']; ?></td>
-					 <?php }else{ ?>
-                                         <td><?php echo $arr_datas['accounte']; ?></td>
-					 <?php } ?>
-
-					
-                                            <td><?php if(isset($arr_datas['category']) && $arr_datas['category']=='move'){ echo $toacc;}else if(isset($arr_datas['label'])){echo $arr_datas['label'];}elseif(isset($arr_datas['otheraccount'])){echo $arr_datas['otheraccount'];}else{echo $arr_datas['address'];} ?></td>
-                                            <td><?php echo $arr_datas['category']; ?></td>
-                                             <td><?php echo $fee; ?></td>
-                                             <td><?php echo $arr_datas['amount']; ?></td>
-                                             <td><?php echo $balafter; ?></td>
-                                              <td><?php if(isset($arr_datas['confirmations'])){echo $arr_datas['confirmations'];} ?></td>
-                                               <td><?php if(isset($arr_datas['txid'])){
-												   if($crypto!='BCH'){ ?>
-								  <a target="_blank" href="https://live.blockcypher.com/{{strtolower($crypto)}}/tx/{{ $arr_datas['txid'] }}">{{ $arr_datas['txid'] }}</a>
-												<?php }else{ ?>
-								  <a target="_blank" href="https://www.blockchain.com/bch/tx/{{ $arr_datas['txid'] }}">{{ $arr_datas['txid'] }}</a>
-												<?php }  
-												   } ?></td>
-                                               <td><?php if(isset($arr_datas['timereceived'])){echo date('Y-m-d H:i:s',$arr_datas['timereceived']);}elseif(isset($arr_datas['time'])){echo date('Y-m-d H:i:s',$arr_datas['time']);} ?></td>
-                                            </tr>
+ ?>
+ <tr><td colspan="10">{{$arr_datas['category']}}</td></tr>
+									 
 									<?php $i++; 
                                     } 
                                     } ?> 
