aef317736d00ff442b8f827527abdcff042bb24b
fatonah83 <54300924+fatonah83@users.noreply.github.com>
2019-09-04 12:28:11 +0800

    edit

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -100,7 +100,7 @@ class AdminController extends Controller
     {
    
 	   $trans = listransaction($crypto, $label);
-	    dd($trans[0]['amount']);
+	    dd($trans[0]);
          return view('admin.transactions_users', Compact('users','crypto','label','trans'));
 
     }
