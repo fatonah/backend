571c3787f6f7da7450b8b78d1a35d99dbbbe4b00
fatonah83 <54300924+fatonah83@users.noreply.github.com>
2019-09-04 11:18:51 +0800

    edit

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -90,7 +90,7 @@ class AdminController extends Controller
   public function listUsers($crypto)
     { 
 		$trans = listransactionall($crypto);	 
- dd($trans[0]);
+ dd($trans[0]['address']);
         return view('admin.list_users', compact('trans','crypto'));
 
     }
