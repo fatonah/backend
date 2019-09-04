af9cd1ea8a7f1e45e773805665fa164393150a48
fatonah83 <54300924+fatonah83@users.noreply.github.com>
2019-09-04 14:07:33 +0800

    edit

--- resources/views/admin/transactions_users.blade.php
+++ resources/views/admin/transactions_users.blade.php
@@ -98,7 +98,7 @@
 									if(isset($trans['fee'])){$fee = $trans['fee'];}else{$fee=0;}
 								  
 
-											if($trans['categoryx']=='move'){ 										   
+											if($trans['category']=='move'){ 										   
 										  $val = substr($trans['amount'], 0, 1);  
 										  if($val=='-'){$fromacc = $trans['account'] ; $toacc = $trans['otheraccount'] ;}
 										  else{$fromacc = $trans['otheraccount'] ; $toacc = $trans['account']; }
