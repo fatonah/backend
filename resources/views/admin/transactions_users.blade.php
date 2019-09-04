740663f34b3503d6befa88392032371c66c4ad91
fatonah83 <54300924+fatonah83@users.noreply.github.com>
2019-09-04 12:43:11 +0800

    edit

--- resources/views/admin/transactions_users.blade.php
+++ resources/views/admin/transactions_users.blade.php
@@ -70,7 +70,7 @@
                                           <?php if($arr_datas['category']=='receive'){ 
 					 $trans = gettransaction_crypto(strtoupper($crypto), $arr_datas['txid']); 
 					 ?>
-                                         <td><?php echo $trans['account']; ?></td>
+                                         <td><?php echo $trans['accountx']; ?></td>
 					 <?php }else if($arr_datas['category']=='move'){ ?>
 										<td><?php echo $fromacc; ?></td>
 					 <?php }else{ ?>
